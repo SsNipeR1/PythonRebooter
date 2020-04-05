07f8d6257f7c266fc24727492bc7a506b2b862bc
Denis Mitin <denis.mitin.005@gmail.com>
2020-04-05 22:23:27 +0300

    v1.0

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo Hello! This program can reboot or power off your device. If you select N, program will exit, if you select P, your phone will powered off, if you select R, your phone will reboot. Good luck! And make sure python is installed.
+su
+git clone https://github.com/SsNipeR1/PythonRebooter.git
+cd /sdcard/PythonRebooter/
+python
+rebooter.py
