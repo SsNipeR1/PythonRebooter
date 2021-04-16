67f97faa775519c4cd096421be4421fce2bb1d52
leonidussaks <42278348+leonidussaks@users.noreply.github.com>
2021-04-16 16:57:28 +0300

    Update rebooter.py

--- rebooter.py
+++ rebooter.py
@@ -1,9 +1,10 @@
 import os
 #rebooter
-choice = input("Write R to reboot, P to power off, N to exit.")
-if choice == P or choice == p:
+print("Write R to reboot, P to power off, N to exit.")
+choice = input(">> ").lower()
+if choice == "p":
     os.system("su && poweroff")
-elif choice == R or choice == r:
+elif choice == "r":
     os.system("su && reboot")
 else:
-    exit()
+    exit() 
